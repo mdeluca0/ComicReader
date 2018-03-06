534e726f318fb79e549e828da5cab0f18dd18f96
Mike <mikedeluca0@gmail.com>
2018-03-06 12:40:18 -0700

    fixed overflowing titles

--- public/modules/volume/volume.js
+++ public/modules/volume/volume.js
@@ -4,5 +4,11 @@ angular.module('volume').controller('volumeCtrl', function($scope, $routeParams,
     var volumeId = $routeParams.id;
     $http.get('/volumes?id='+volumeId).then(function (response) {
         $scope.volume = response.data[0];
+        for (var i = 0; i < $scope.volume.issues.length; i++) {
+            $scope.volume.issues[i].display_name = $scope.volume.issues[i].name.substr(0, 20);
+            if ($scope.volume.issues[i].name.length > 20) {
+                $scope.volume.issues[i].display_name += '...';
+            }
+        }
     });
 });
